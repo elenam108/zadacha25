0c736056201160a09b284a2b250c68244c23e84f
ElenaM <elenam108@yandex.ru>
2022-11-05 21:08:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Pov = 1;
+// for (int i = 1; i <= b; i++) Pov = Pov * a;
+// Console.WriteLine($"Число {a} в степени {b} равно {Pov}");
+
+ void PovNumbers(int x)
+ {
+    int Pov = 1;
+    for (int i = 1; i <= b; i++) 
+    Pov = Pov * a;
+    Console.WriteLine($"Число {a} в степени {b} равно {Pov}");
+}
+int PovNumbers2(int x)
+{
+int Pov = 1;
+    for (int i = 1; i <= b; i++) 
+    Pov = Pov * a;
+
+return Pov;
+}
+Console.WriteLine(PovNumbers2(Pov));
